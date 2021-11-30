b4bc1da0b74a01247d708ef87490d5085b8570c7
Mark <11mark1@mail.ru>
2021-11-30 22:10:31 +0300

    first commit

---
+++ JS/dcript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
